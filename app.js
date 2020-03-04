6f1895f2b0747830ef8680779074e1634c4bf703
nadimahmadulhoq <nadimahmadulhoq@gmail.com>
2020-03-04 15:59:42 +0600

    initial commit.

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send("welcome...");
+});
+
+// const PORT = process.env.PORT || 3000;
+app.listen(3000, console.log('server running.'));
